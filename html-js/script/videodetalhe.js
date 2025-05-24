22c317d3554d3bb8008e075884795cdb16bbcff4
Felipe Pacheco <felipebpacheco2018@gmail.com>
2025-05-24 15:33:31 -0300

    descrição fundo escuro

--- html-js/script/videodetalhe.js
+++ html-js/script/videodetalhe.js
@@ -3,6 +3,7 @@
 // "Editar" leva ao form com id;
 // "Arquivo" mostra o arquivo de vídeo salvo para reprodução com ferramentas externas.
 // Cabeçalho e botões fixos na tela;
+// Source não carrega o arquivo até o vídeo;
 
 const player = document.querySelector("video");
 const fonte = document.querySelector("source");

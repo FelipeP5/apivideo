970a42606263dfcb0e97a87d573191255623a9cb
Felipe Pacheco <felipebpacheco2018@gmail.com>
2025-06-05 11:43:30 -0300

    msg de erro na falha em carregar vídeo

--- html-js/script/videodetalhe.js
+++ html-js/script/videodetalhe.js
@@ -1,11 +1,14 @@
 // Descrição colapsável
 // "Incluso" exibe playlists que o incluem;
-// "Arquivo" mostra o arquivo de vídeo salvo para reprodução com ferramentas externas.
 
+const areaDePlayer = document.getElementById("area-de-player");
 const player = document.querySelector("video");
 const nome = document.getElementById("nome");
 const descricao = document.getElementById("descricao");
 const data = document.getElementById("data");
+const erroNoPlayer = document.getElementById("video-el-nao-sustentado");
+const msgErro = document.getElementById("msg-erro");
+const download = document.getElementById("arquivo-nao-carregou");
 const videoURL = "http://127.0.0.1:8000/video/";
 const playlistURL = "http://127.0.0.1:8000/playlist/";
 const relacoes = "http://127.0.0.1:8000/playlistvideo/";
@@ -25,12 +28,14 @@ fetch(videoURL + id + "/")
 
 function preencherFonte(arquivo){
     console.log(arquivo);
-    const fonte = document.createElement("source");
-    fonte.setAttribute("src", arquivo);
-    player.appendChild(fonte);
-    console.log(fonte, "#############", player);
+    player.setAttribute("src", arquivo);
     player.load();
-
+    player.addEventListener("error", arquivo => {
+        areaDePlayer.removeChild(player);
+        erroNoPlayer.href = arquivo;
+        msgErro.classList.remove("d-none");
+        download.href = `./videoform.html?id=${id}`;
+    })
 };
 
 function editarVideo(){

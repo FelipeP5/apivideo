6b351409825b8a8e9d0ad2407167deb404389195
Felipe Pacheco <felipebpacheco2018@gmail.com>
2025-06-10 06:16:23 -0300

    login.html feito

--- html-js/script/login.js
+++ html-js/script/login.js
@@ -3,13 +3,34 @@ const closeBtn = document.getElementById("closeBtn");
 const formLogin = document.getElementById("formLogin");
 const formCadastrar = document.getElementById("formCadastrar");
 const cadastrarShow = document.getElementById("castrarShow");
-const usuarioLoginInput = document.getElementById("usuarioLoginInput");
-const senhaLoginInput = document.getElementById("senhaLoginInput");
-const usuarioCadastrarInput = document.getElementById("usuarioCadastrarInput");
-const senhaCadastrarInput = document.getElementById("senhaCadastrarInput");
+const usuarioLogin = document.getElementById("usuarioLoginInput");
+const senhaLogin = document.getElementById("senhaLoginInput");
+const usuarioCadastrar = document.getElementById("usuarioCadastrarInput");
+const senhaCadastrar = document.getElementById("senhaCadastrarInput");
 const entrarBtn = document.getElementById("entrarBtn");
 const cadastrarBtn = document.getElementById("cadastrarBtn");
 
 cadastrarShow.addEventListener("click", () => cadastrarDialog.showModal());
+closeBtn.addEventListener("click", () => cadastrarDialog.close());
+formCadastrar.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    cadastrarDialog.close();
+    const usuarioDadosEntrada = {
+        usuario : usuarioCadastrar.value,
+        senha : senhaCadastrar.value,
+    };
+    console.log(JSON.stringify(usuarioDadosEntrada));
+    localStorage.setItem("usuarioDados", JSON.stringify(usuarioDadosEntrada));
+})
 
-closeBtn.addEventListener("click", () => cadastrarDialog.close())
+entrarBtn.addEventListener("click", () => {
+    const usuarioDadosSaida = JSON.parse(localStorage.getItem("usuarioDados"));
+    if (usuarioLogin.value === usuarioDadosSaida.usuario && senhaLogin.value === usuarioDadosSaida.senha){
+        sessionStorage.setItem("autenticado", true);
+        location.href = "./";
+    }
+    else {
+        console.log(`Usuário: ${usuarioLogin.value}, Senha: ${senhaLogin.value}.
+        São desiguais à ${usuarioDadosSaida.usuario} e ${usuarioDadosSaida.senha} de ${usuarioDadosSaida}`);
+    }
+});

b54af11d7c2288c6219b8f7904bea24ed9643c1a
felip2 <felipebpacheco2018@gmail.com>
2025-06-13 18:13:43 -0300

    feat: btn de sair e cancelar no usuario

--- html-js/script/usuario.js
+++ html-js/script/usuario.js
@@ -11,6 +11,7 @@ function exibirUsuario(){
         <p class="fs-2 fw-bold">${dadosUsuario.usuario}</p>
         <p class="fs-4">Senha: ${dadosUsuario.senha}</p>
         <button id="redefinirBtn" type="button" class="rounded p-2 bg-csecondary clr-cprimary">Redefinir</button>
+        <a href="./login.html" class=" btn btn-danger fst-none rounded ms-sm-5">Sair</a>
     `;
     const redefinirBtn = document.getElementById("redefinirBtn");
     redefinirBtn.addEventListener("click", redefinirUsuario);
@@ -21,16 +22,23 @@ function redefinirUsuario(){
         <p><input id="novoUsuarioInput" type="text"></p>
         <p>Senha: <input id="novaSenhaInput" type="password"></p>
         <button id="confirmarBtn" type="button" class="rounded p-2 bg-csecondary clr-cprimary">Confirmar</button>
+        <button id="cancelarBtn" type="button" class="rounded p-2 bg-cprimary clr-csecondary">Cancelar</button>
     `;
     const novoUsuarioInput = document.getElementById("novoUsuarioInput");
     const novaSenhaInput = document.getElementById("novaSenhaInput");
     const confirmarBtn = document.getElementById("confirmarBtn");
+    const cancelarBtn = document.getElementById("cancelarBtn");
 
     confirmarBtn.addEventListener("click", () => {
         dadosUsuario.usuario = novoUsuarioInput.value;
         dadosUsuario.senha = novaSenhaInput.value;
         console.log(dadosUsuario);
-        localStorage.setItem("dadosUsuario", dadosUsuario);
+        localStorage.setItem("dadosUsuario", JSON.stringify(dadosUsuario));
+        exibirUsuario();
+    });
+
+    cancelarBtn.addEventListener("click", () => {
+        console.log("Cancelar");
         exibirUsuario();
     })
 }

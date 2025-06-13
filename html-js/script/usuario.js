a77fe49adead28b7f05472010a3a0400ef027581
felip2 <felipebpacheco2018@gmail.com>
2025-06-13 17:49:35 -0300

    feat: página de usuário

--- html-js/script/usuario.js
+++ html-js/script/usuario.js
@@ -1,1 +1,36 @@
-location.href.replace("login.html");
+const tituloEl = document.querySelector("title");
+const blocoUsuario = document.getElementById("bloco-usuario");
+const dadosUsuario = JSON.parse(localStorage.getItem("dadosUsuario"));
+
+tituloEl.innerText = `${dadosUsuario.usuario}`;
+
+exibirUsuario();
+
+function exibirUsuario(){
+    blocoUsuario.innerHTML = `
+        <p class="fs-2 fw-bold">${dadosUsuario.usuario}</p>
+        <p class="fs-4">Senha: ${dadosUsuario.senha}</p>
+        <button id="redefinirBtn" type="button" class="rounded p-2 bg-csecondary clr-cprimary">Redefinir</button>
+    `;
+    const redefinirBtn = document.getElementById("redefinirBtn");
+    redefinirBtn.addEventListener("click", redefinirUsuario);
+}
+
+function redefinirUsuario(){
+    blocoUsuario.innerHTML = `
+        <p><input id="novoUsuarioInput" type="text"></p>
+        <p>Senha: <input id="novaSenhaInput" type="password"></p>
+        <button id="confirmarBtn" type="button" class="rounded p-2 bg-csecondary clr-cprimary">Confirmar</button>
+    `;
+    const novoUsuarioInput = document.getElementById("novoUsuarioInput");
+    const novaSenhaInput = document.getElementById("novaSenhaInput");
+    const confirmarBtn = document.getElementById("confirmarBtn");
+
+    confirmarBtn.addEventListener("click", () => {
+        dadosUsuario.usuario = novoUsuarioInput.value;
+        dadosUsuario.senha = novaSenhaInput.value;
+        console.log(dadosUsuario);
+        localStorage.setItem("dadosUsuario", dadosUsuario);
+        exibirUsuario();
+    })
+}

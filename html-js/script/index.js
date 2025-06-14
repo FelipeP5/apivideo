c36571ec77ae345d3500b1e22e5898eff711c60e
felip2 <felipebpacheco2018@gmail.com>
2025-06-14 10:36:13 -0300

    pesquisa básica

--- html-js/script/index.js
+++ html-js/script/index.js
@@ -3,29 +3,30 @@
 //Opção de filtrar entre video e playlist;
 
 if(JSON.parse(sessionStorage.getItem("autenticado")) !== true){location.replace("login.html")};
-document.getElementById("tema").addEventListener("click", () => {
-    if (localStorage.getItem("tema" === null || undefined || "claro")){
-        localStorage.setItem("tema", "escuro");
-    } else {
-        localStorage.setItem("tema", "claro");
-    }
-});
+// document.getElementById("tema").addEventListener("click", () => {
+//     if (localStorage.getItem("tema" === null || undefined || "claro")){
+//         localStorage.setItem("tema", "escuro");
+//     } else {
+//         localStorage.setItem("tema", "claro");
+//     }
+// });
 
-if(localStorage.getItem("tema") === "escuro"){
-    
-}
+// if(localStorage.getItem("tema") === "escuro"){
+
+// }
 
 const tituloH3 = document.getElementById("titulo");
 const placeholderImg = "../svg/placeholder-img.jpg";
 const videoURL = "http://127.0.0.1:8000/video/";
 const playlistURL = "http://127.0.0.1:8000/playlist/";
 const relacoes = "http://127.0.0.1:8000/playlistvideo/";
+const videoNav = document.getElementById("video-nav");
+const playlistNav = document.getElementById("playlist-nav");
 const lista = document.getElementById("lista");
+const pesquisaInput = document.getElementById("pesquisa-input");
 const escolhaCriacao = document.getElementById("escolha-criacao");
-
-if (lista.children !== null){
-    tituloH3.innerText = "Alterações Recentes";
-}
+const param = new URLSearchParams(location.search).get("r");
+let itensSalvos = [];
 
 fetch(videoURL)
     .then(res => res.json())
@@ -36,47 +37,54 @@ fetch(playlistURL)
     .then(playlists => listePlaylists(playlists))
     .catch(erro => console.warn(erro, "Erro ao carregar playlists"));
 
+if (lista.children === 1){
+    tituloH3.innerText = "Alterações Recentes";
+    }
+    
+//
+document.getElementById("search-btn").addEventListener("click", filtroPesquisa)
+
+videoNav.addEventListener("click", () => location.search = "r=playlist"); //r indica remoção
+playlistNav.addEventListener("click", () => location.search = "r=video");
+
+
 function listeVideos(videos){
     videos.forEach(video => {
-        const cartao = `<div id="video${video.id}" class="col">
-                            <div onclick="videodetalhe(${video.id})" class="pointer card bg-cprimary clr-csecondary">
-                                <img src="${video.thumbnail ? video.thumbnail : placeholderImg}" alt="Nenhuma imagem" class="card-img-top img-fluid custom-img bg-csecondary">
-                                <div class="card-img-overlay">
-                                        <div class="container p-0">
-                                            <a href="videoform.html?id=${video.id}" class="ms-auto btn clr-cprimary">Editar</a>
-                                            <button type="button" onclick="excluirVideo(${video.id})" class="btn clr-cprimary">Excluir</button>
-                                        </div>
-                                </div>
-                                <h6 class="card-header text-center">${video.nome}</h6>
-                            </div>
-                        </div>
-                    `;
-        lista.innerHTML += cartao;
+        itensSalvos.push({
+            tipo: "video",
+            nome: video.nome,
+            obj: video,
+        });
     });
 };
 
 function listePlaylists(playlists){
     playlists.forEach(playlist => {
-        const cartao = `
-                <div id="playlist${playlist.id}" class="col">
-                     <div onclick="playlistdetalhe(${playlist.id})" class="pointer card bg-cprimary clr-csecondary">
-                            <img src="${playlist.thumbnail ? playlist.thumbnail : placeholderImg}" alt="image cap" class="card-img-top img-fluid custom-img bg-csecondary">
-                            <div class="card-img-overlay">
-                                <div class="container p-0">
-                                    <a href="playlistform.html?id=${playlist.id}" class="ms-auto btn clr-cprimary">Editar</a>
-                                    <a href="#" onclick="excluirPlaylist(${playlist.id})" class="btn clr-cprimary">Excluir</a>
-                                </div>
-                            </div>
-                            <h6 class="card-header text-center">${playlist.nome}</h6>
-                        </div>   
-                </div>
-                    `;
-        lista.innerHTML += cartao;
+        
+        itensSalvos.push({
+            tipo: "playlist",
+            nome: playlist.nome,
+            obj: playlist,
+        })
     })
 };
 
+function filtroPesquisa(){
+    let itensSelecionados = [];
+
+    const query = pesquisaInput.value;
+    let itens = itensSalvos.filter( item => item.tipo !== param);
+    let queryset = itens = itens.filter(item => item.nome === query);
+    itens.forEach(item => itensSelecionados.push(item));
+    
+    return exibirSelecionados(queryset);
+}
+
+function exibirSelecionados(selecao){
+    console.log(selecao);
+}
+
 function escolhaCriacaoMenu(){
-    console.log("Evento click escutado")
     escolhaCriacao.showModal();
 }
 

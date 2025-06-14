7ea0fe8473eeba5c3b989616e79356436409e177
felip2 <felipebpacheco2018@gmail.com>
2025-06-14 13:53:50 -0300

    filtragens feitas

--- html-js/script/index.js
+++ html-js/script/index.js
@@ -25,27 +25,32 @@ const playlistNav = document.getElementById("playlist-nav");
 const lista = document.getElementById("lista");
 const pesquisaInput = document.getElementById("pesquisa-input");
 const escolhaCriacao = document.getElementById("escolha-criacao");
-const param = new URLSearchParams(location.search).get("r");
+const param = new URLSearchParams(location.search).get("rm");
 let itensSalvos = [];
 
-fetch(videoURL)
-    .then(res => res.json())
-    .then(videos => listeVideos(videos))
-    .catch(erro => console.warn(erro, "Erro ao carregar vídeos"));
-fetch(playlistURL)
-    .then(res => res.json())
-    .then(playlists => listePlaylists(playlists))
-    .catch(erro => console.warn(erro, "Erro ao carregar playlists"));
+async function todos(){
+    fetch(videoURL)
+        .then(res => res.json())
+        .then(videos => listeVideos(videos))
+        .catch(erro => console.warn(erro, "Erro ao carregar vídeos")),
 
-if (lista.children === 1){
+    await fetch(playlistURL)
+        .then(res => res.json())
+        .then(playlists => listePlaylists(playlists))
+        .catch(erro => console.warn(erro, "Erro ao carregar playlists"));
+    
+    filtroPesquisa(itensSalvos);
+}
+
+if (lista.children.length > 1){
     tituloH3.innerText = "Alterações Recentes";
     }
     
 //
-document.getElementById("search-btn").addEventListener("click", filtroPesquisa)
+pesquisaInput.addEventListener("input", filtroPesquisa)
 
-videoNav.addEventListener("click", () => location.search = "r=playlist"); //r indica remoção
-playlistNav.addEventListener("click", () => location.search = "r=video");
+videoNav.addEventListener("click", () => location.search = "rm=playlist");
+playlistNav.addEventListener("click", () => location.search = "rm=video");
 
 
 function listeVideos(videos){
@@ -69,19 +74,82 @@ function listePlaylists(playlists){
     })
 };
 
-function filtroPesquisa(){
-    let itensSelecionados = [];
-
+function filtroPesquisa(itensSalvos){
+    let itensSelecionados = itensSalvos.filter(item => item.tipo !== param);
+    console.log("await superado")
     const query = pesquisaInput.value;
-    let itens = itensSalvos.filter( item => item.tipo !== param);
-    let queryset = itens = itens.filter(item => item.nome === query);
-    itens.forEach(item => itensSelecionados.push(item));
+    const queryWords = query.split(" ");
+    const queryChars = query.split("");
+
+    itensSelecionados.map((itemObj) => {
+        itemObj.peso = 0;
+        for (const queryWord of queryWords){
+           if (itemObj.nome.includes(queryWord)){
+            itemObj.peso += 30;
+           }
+           if (itemObj.nome.includes(queryWord.toLowerCase())){
+            itemObj.peso += 25;
+           }
+        };
+        for (const queryChar of queryChars){
+            if (itemObj.nome.includes(queryChar)){
+             itemObj.peso += 10;
+            }
+            if (itemObj.nome.includes(queryChar.toLowerCase())){
+                itemObj.peso += 5;
+            }
+         }
+        return itemObj;
+    });
+    itensSelecionados.sort((itemObjA, itemObjB) => itemObjB.peso - itemObjA.peso);
     
-    return exibirSelecionados(queryset);
+    return exibirSelecionados(itensSelecionados);
 }
 
 function exibirSelecionados(selecao){
-    console.log(selecao);
+    console.log("Seleção ", selecao);
+    lista.innerHTML = `
+        <div onclick="escolhaCriacaoMenu()" class="col pointer">
+                    <div class="h-100 card align-items-center justify-content-evenly">
+                        <img src="../svg/red-plus-11961.svg" alt="+" class="w-25 h-25">
+                    </div>
+        </div>
+    `;
+    selecao.forEach(item => {
+        if (item.tipo === "video"){
+            const cartaoVideo = `<div id="video${item.obj.id}" class="col">
+                            <div onclick="videodetalhe(${item.obj.id})" class="pointer card bg-cprimary clr-csecondary">
+                                <img src="${item.obj.thumbnail || placeholderImg}" alt="Nenhuma imagem" class="card-img-top img-fluid custom-img bg-csecondary">
+                                <div class="card-img-overlay">
+                                        <div class="container p-0">
+                                            <a href="videoform.html?id=${item.obj.id}" class="ms-auto btn clr-cprimary">Editar</a>
+                                            <button type="button" onclick="excluirVideo(${item.obj.id})" class="btn clr-cprimary">Excluir</button>
+                                        </div>
+                                </div>
+                                <h6 class="card-header text-center">${item.nome}</h6>
+                            </div>
+                        </div>
+                    `;
+        lista.innerHTML += cartaoVideo;
+        }
+        else {
+            const cartaoPlay = `
+                <div id="playlist${item.obj.id}" class="col">
+                    <div onclick="playlistdetalhe(${item.obj.id})" class="pointer card bg-cprimary clr-csecondary">
+                            <img src="${item.obj.thumbnail || placeholderImg}" alt="image cap" class="card-img-top img-fluid custom-img bg-csecondary">
+                            <div class="card-img-overlay">
+                                <div class="container p-0">
+                                    <a href="playlistform.html?id=${item.obj.id}" class="ms-auto btn clr-cprimary">Editar</a>
+                                    <a href="#" onclick="excluirPlaylist(${item.obj.id})" class="btn clr-cprimary">Excluir</a>
+                                </div>
+                            </div>
+                            <h6 class="card-header text-center">${item.nome}</h6>
+                        </div>   
+                </div>
+            `;
+            lista.innerHTML += cartaoPlay;
+        }
+    })
 }
 
 function escolhaCriacaoMenu(){
